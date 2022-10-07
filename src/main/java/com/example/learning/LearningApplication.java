062c813d1cfb74eb3e45a5491cb04b71df8d4b81
Junjie Chen <chen2468830035@gmail.com>
2022-10-07 04:43:14 +0200

    update

--- src/main/java/com/example/learning/LearningApplication.java
+++ src/main/java/com/example/learning/LearningApplication.java
@@ -2,6 +2,8 @@ package com.example.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class LearningApplication {
@@ -9,6 +11,11 @@ public class LearningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
